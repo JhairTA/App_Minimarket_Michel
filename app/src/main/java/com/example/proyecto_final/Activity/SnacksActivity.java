ecb40dbc5de562511f4868bb42f52b944cd251d6
JhairTA <jhairtiradoatalaya@gmail.com>
2023-04-13 22:18:22 -0500

    avance2

--- app/src/main/java/com/example/proyecto_final/Activity/SnacksActivity.java
+++ app/src/main/java/com/example/proyecto_final/Activity/SnacksActivity.java
@@ -42,6 +42,21 @@ public class SnacksActivity extends AppCompatActivity {
         recyclerViewProductoGalletas();
     }
 
+    public void buscar(View view){
+        Intent intent =new Intent(this, BuscadorActivity.class);
+        startActivity(intent);
+    }
+
+    public void inicio(View view){
+        Intent intent =new Intent(this, InicioActivity.class);
+        startActivity(intent);
+    }
+
+    public void menu(View view){
+        Intent intent =new Intent(this, MenuActivity.class);
+        startActivity(intent);
+    }
+
     private void recyclerViewCategoria() {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         recyclerViewCategoriaList = findViewById(R.id.rv1);
@@ -123,16 +138,16 @@ public class SnacksActivity extends AppCompatActivity {
         recyclerViewProductoGalletasList.setLayoutManager(linearLayoutManager);
 
         ArrayList<ProductoGeneral> productoGalletasList = new ArrayList<>();
-        productoGalletasList.add(new ProductoGeneral("Yogurt Gloria Fresa Botella 1Kg","yogurt_gloria","Es una cremosa y deliciosa bebida láctea con trozos de fruta. Es un buen alimento para niños y adultos porque contiene los nutrientes naturales de la leche.",9.50));
-        productoGalletasList.add(new ProductoGeneral("Yogurt Griego Laive Botella 200gr","yogurt_griego","Con la perfecta combinación de cremosidad, textura y deliciosos sabores  40g de proteína en una botella. SIN azúcar añadida.",7.90));
-        productoGalletasList.add(new ProductoGeneral("Yogurt Laive Fresa Botella 1Kg","yogurt_laive","Es el mejor yogurt para tomar en el día ya que contiene 1,000 millones de probioticos por cada vaso de 200gr, que ayudan a mejorar tu digestión, fortaceler tus defensas y absorber mejor los nutrientes.",5.50));
-        productoGalletasList.add(new ProductoGeneral("Yogurt Bells Fresa Botella 1Kg","yogurt_bells","Tus defensas MÁS FUERTES que nunca, con 1000 millones de probióticos en una botella que ayudan a reforzar tus defensas y tu sistema inmunológico.",10.30));
-        productoGalletasList.add(new ProductoGeneral("Yogurt Actibio Linaza Fresa Botella 1Kg","yogurt_actibio","Contiene calcio, proteínas y otros nutrientes importantes para la salud. El calcio es esencial para la salud de los huesos y los dientes, mientras que las proteínas son necesarias para la formación de músculos, tejidos y células del cuerpo.",7.60));
-        productoGalletasList.add(new ProductoGeneral("Yogurt Sbelt Fresa Botella 1Kg","yogurt_sbelt","Yogurt Laive Sbelt con 0% azúcar añadida, 0% grasa, con probióticos y fibra. ¡Es el aliado perfecto que necesitas para verte y sentirte bien!.",9.90));
-        productoGalletasList.add(new ProductoGeneral("Yogurt Yoleit Fresa Botella 1Kg","yogurt_yoleit","Yogurt bebible sabor natural con cultivos probioticos",7.50));
-        productoGalletasList.add(new ProductoGeneral("Yogurt Piamonte Botella 946ml","yogurt_piamonte","Yogurt parcialmente descremado totalmente natural, sin azucar",15.40));
-        productoGalletasList.add(new ProductoGeneral("Yogurt Milkito Fresa Botella 1Kg","yogurt_milkito","Yogurt parcialmente descremado elaborado con leche de vaca. Es una rica fuente de proteínas que ayuda a formar los músculos. Además, su aporte de calcio contribuye a mantener una buena salud ósea. Es perfecto para niños y adultos que quieren disfrutar de un yogurt cremoso y nutritivo.",10.70));
-        productoGalletasList.add(new ProductoGeneral("Yogurt Slim Fresa Botella 1Kg","slim2","Este yogurt descremado edulcorado es un producto 0% grasas trans y sin azúcares añadidos. Con proteínas, vitaminas E, un potente antioxidante, y vitamina D. Está pensado para mujeres y hombres de 20 a 50 años de edad que quieran reducir el consumo de calorías, azúcares y grasas.",6.50));
+        productoGalletasList.add(new ProductoGeneral("Galleta Casino Black Canela X 261g","galleta_casino","hghfgg",3.50));
+        productoGalletasList.add(new ProductoGeneral("Galleta Picaras Chocolate 6un","galletas_picaras","gfgdfgh",6.30));
+        productoGalletasList.add(new ProductoGeneral("Galleta Morochas Paquete 6un","galleta_morochas","ghhgh",4.80));
+        productoGalletasList.add(new ProductoGeneral("Galleta Tentación Naranja Paquete 6un","galleta_tentacion","gfhfgh",5.20));
+        productoGalletasList.add(new ProductoGeneral("Galleta Chomp Chocolate Paquete 6un","galleta_chomp","gfgfgg",4.10));
+        productoGalletasList.add(new ProductoGeneral("Galleta Margarita Sayon Paquete 6un","galleta_margarita","dfgdg",2.70));
+        productoGalletasList.add(new ProductoGeneral("Galleta Oreo Original X 432g","galleta_oreo","rtrtr",6.30));
+        productoGalletasList.add(new ProductoGeneral("Galleta Ritz Queso Six Pack","galleta_ritz","fvcff",2.50));
+        productoGalletasList.add(new ProductoGeneral("Wafer Nik Vainilla Paquete 6un","galleta_nik","fgfgfds",1.90));
+        productoGalletasList.add(new ProductoGeneral("Galleta Glacitas Paquete 6un","galletas_glasitas","yujhgjj",2.99));
 
         adapter3=new ProductoSnacksAdaptor(productoGalletasList);
         recyclerViewProductoGalletasList.setAdapter(adapter3);
