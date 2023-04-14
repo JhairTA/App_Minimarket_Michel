dcfb408c32c23d1628323ddbbd1468c357fa3b14
JhairTA <jhairtiradoatalaya@gmail.com>
2023-04-14 13:03:45 -0500

    avance3

--- app/src/main/java/com/example/proyecto_final/Activity/CuidadoPersonalActivity.java
+++ app/src/main/java/com/example/proyecto_final/Activity/CuidadoPersonalActivity.java
@@ -6,7 +6,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.proyecto_final.Adaptor.CategoryAdaptor;
+import com.example.proyecto_final.Adaptor.ProductoCuidadoBucalAdaptor;
+import com.example.proyecto_final.Adaptor.ProductoYogurtsAdaptor;
 import com.example.proyecto_final.Domain.CategoryDomain;
+import com.example.proyecto_final.Domain.ProductoGeneral;
+import com.example.proyecto_final.Domain.ProductoYogurts;
 import com.example.proyecto_final.R;
 
 import android.content.Intent;
@@ -20,7 +24,9 @@ public class CuidadoPersonalActivity extends AppCompatActivity {
 
     private Toolbar toolbar1;
 
-    private RecyclerView recyclerViewCategoriaList;
+    private RecyclerView.Adapter adapter, adapter2, adapter3;
+
+    private RecyclerView recyclerViewCategoriaList, recyclerViewProductoCuidadoBucalList, recyclerViewProductoLechesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +38,7 @@ public class CuidadoPersonalActivity extends AppCompatActivity {
         setSupportActionBar(toolbar1);
 
         recyclerViewCategoria();
+        recyclerViewProductoCuidadoBucal();
     }
 
     private void recyclerViewCategoria() {
@@ -86,5 +93,26 @@ public class CuidadoPersonalActivity extends AppCompatActivity {
             }
         });
         recyclerViewCategoriaList.setAdapter(adapter);
+    }
+
+    private void recyclerViewProductoCuidadoBucal(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewProductoCuidadoBucalList = findViewById(R.id.rv2);
+        recyclerViewProductoCuidadoBucalList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<ProductoGeneral> productoCuidadoBucalList = new ArrayList<>();
+        productoCuidadoBucalList.add(new ProductoGeneral("Pasta Dental Colgate Paquete 3un", "cu_colgate", "fdsf",18.00));
+        productoCuidadoBucalList.add(new ProductoGeneral("Listerine Enjuague Bucal 500ml", "cu_listerine", "fdsf",17.00));
+        productoCuidadoBucalList.add(new ProductoGeneral("Hilo Dental Oral-B X 1 Unidad", "cu_hilo", "fdsf",11.75));
+        productoCuidadoBucalList.add(new ProductoGeneral("Cepillo Dental Colgate 3 Unidades", "cu_cepillo", "fdsf",15.20));
+        productoCuidadoBucalList.add(new ProductoGeneral("Pasta Dental Oral-B Caja 75ml", "cu_oralb", "fdsf",12.50));
+        productoCuidadoBucalList.add(new ProductoGeneral("Colgate Enjuague Bucal X 500ml", "cu_colgateenjuage", "fdsf",17.58));
+        productoCuidadoBucalList.add(new ProductoGeneral("Pasta Dental Dento Tubo 75ml", "cu_dento", "fdsf",15.20));
+        productoCuidadoBucalList.add(new ProductoGeneral("Cepillo Electrico Oral-B Caja 1un", "cu_cepilloelectrico", "fdsf",59.90));
+        productoCuidadoBucalList.add(new ProductoGeneral("Dento Enjuague Bucal Menta 500ml", "cu_dentoenjuage", "fdsf",14.50));
+        productoCuidadoBucalList.add(new ProductoGeneral("Hilo Dental Colgate X 1 Unidad", "cu_colgatehilo", "fdsf",13.19));
+
+        adapter2=new ProductoCuidadoBucalAdaptor(productoCuidadoBucalList);
+        recyclerViewProductoCuidadoBucalList.setAdapter(adapter2);
     }
 }
